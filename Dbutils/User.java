57e83d0079057cac4a50d0a1fb6d4e2f7f5b2960
cwbbc <31918313+cwbbc@users.noreply.github.com>
2017-11-17 19:59:25 +0800

    Create User.java

---
+++ Dbutils/User.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pers.bean;
+
+/**
+ *
+ * @author 伯通
+ */
+public class User {
+  public  String name;
+  public  String password;
+  public  int id;
+
+}
